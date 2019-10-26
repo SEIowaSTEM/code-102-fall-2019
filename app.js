971195d7a0127aa56e807feb21d2e9103cbc3154
SEIowaSTEM <seiowa.stem@gmail.com>
2019-10-26 15:27:55 -0500

    Final push of new code

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 
 
+day1AM();
+day1PM();
+
 function setup() {
     createCanvas(640, 480);
 }
@@ -27,7 +30,7 @@ function day1AM() {      // declares function you make
     var inches = heightInInches % 12; //% sign instead of divide sign gives you back the remainder (also known as modules)
     var address = "3971 Roberts Rdg NE";
     var middleName; //don't need to define a variable
-   
+
     console.log("Hello World");
     console.log(address); //check to see that the console prints the correct info
     console.log(typeof (address)); //tells you the type of this variable
@@ -46,40 +49,69 @@ function day1AM() {      // declares function you make
     console.log(fullName + " is " + feet + " feet " + inches + " inches tall.") //this helps you confirm your formulas worked  
 }
 
-function print(input){
+function print(input) {
     console.log(input);
 }
 
 //Converts Fahrenheit to Celsius
 //Input: temperature in Fahrenheit 
-function f2c(tempF){
-    return((tempF -32)*(5/9)); //returns the answer to f2c
+function f2c(tempF) {
+    return ((tempF - 32) * (5 / 9)); //returns the answer to f2c
 }
 
-function bmiConverter(weight,heightInInches){
-    return((weight/heightInInches)*703);
+function bmiConverter(weight, heightInInches) {
+    return ((weight / heightInInches) * 703);
 }
 
-function canIGoToTheCasino(age, location){
-    return age >= 21  && location == "IA" || age >= 18 && location == "MN"; //combine two conditions together and both sides have to be two; || means or statement
+function canIGoToTheCasino(age, location) {
+    return age >= 21 && location == "IA" || age >= 18 && location == "MN"; //combine two conditions together and both sides have to be two; || means or statement
 }
 
 
 
-function day1PM(){
+function day1PM() {
     var convertedTemp = f2c(212); //calls f2c function which then prints the conversion
-    print (convertedTemp);
+    print(convertedTemp);
     var age = 19;
     var bmiConverterOutput = bmiConverter(130, 69);
-    print (bmiConverterOutput);
-    print (canIGoToTheCasino (age, "IA"));
-    print (canIGoToTheCasino (19, "MN"));
-    
+    print(bmiConverterOutput);
+    print(canIGoToTheCasino(age, "IA"));
+    print(canIGoToTheCasino(19, "MN"));
+    print("Kristine's Choice");
+    print(canISleepIn("Sunday", 18));
+    print("Abby's Choice");
+    print(canISleepIn("Monday", 14));
+    print("Mark's Choice");
+    print(canISleepIn("Tuesday", 10));
+    print("Tempertature is 35 degrees so wear " + whatShouldIWear(35));
+    print("Tempertature is 95 degrees so wear " + whatShouldIWear(95));
+
+
+}
+
+function canISleepIn(day, hour) {
+    var day;
+    var hour;
+    return day == "Saturday" || day == "Sunday" || hour <= 8 && hour >= 17;
 }
 
 
- day1AM();
- day1PM();
+function whatShouldIWear(temp) {
+    if (temp >= 75) {
+        return "bathing suit";
+    }
+    else if (temp >= 45) {
+        return "Light Jacket";
+    }
+    else if (temp >= 10) {
+        return "Winter Jacket";
+    }
+    else {
+        return "Down Jacket"
+    }
+}
+
+
 
 
 
